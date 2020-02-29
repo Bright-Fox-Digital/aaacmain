ce553a74c93aca10b0f4d7a452ca0945c38f0257
Pixel Devs <admin@pixeldevs.io>
2020-02-29 16:48:39 -0500

    update footer

--- footer.php
+++ footer.php
@@ -18,10 +18,9 @@
 			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'aaac' ) ); ?>">
 				<?php
 				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'aaac' ), 'WordPress' );
+				//printf( esc_html__( 'Proudly powered by %s', 'aaac' ), 'WordPress' );
 				?>
 			</a>
-			<span class="sep"> | </span>
 				<?php
 				/* translators: 1: Theme name, 2: Theme author. */
 				printf( esc_html__( 'Theme: %1$s by %2$s.', 'aaac' ), 'aaac', '<a href="https://pixeldevs.io">PixelDevs</a>' );
